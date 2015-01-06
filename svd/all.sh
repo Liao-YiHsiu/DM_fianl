f03010600afe7c16ccb81cf5d81cbe0c8484a435
Yi-Hsiu Liao <nee0224256667@yahoo.com.tw>
2015-01-06 20:48:52 +0800

    precision = 117 / 1000 , 0.117000 recall    = 117 / 9468 , 0.012357 f-measure = 0.022354 precision = 150 / 1000 , 0.150000 recall    = 150 / 12144 , 0.012352 f-measure = 0.022824 precision = 102 / 1000 , 0.102000 recall    = 102 / 8183 , 0.012465 f-measure = 0.022215\n svd N=?? test

--- svd/all.sh
+++ svd/all.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+
+	matlab -r "run('../graph.txt', '../training.txt', '../test_data/test_data_q1.txt', 'out1.txt');exit(0);" -nodesktop -nosplash 
+	matlab -r "run('../graph.txt', '../training.txt', '../test_data/test_data_q2.txt', 'out2.txt');exit(0);" -nodesktop -nosplash
+	matlab -r "run('../graph.txt', '../training.txt', '../test_data/test_data_q3.txt', 'out3.txt');exit(0);" -nodesktop -nosplash
+
+	python ../eval.py out1.txt ../test_data/test_data_a1.txt | tee result.txt
+	python ../eval.py out2.txt ../test_data/test_data_a2.txt | tee -a result.txt
+	python ../eval.py out3.txt ../test_data/test_data_a3.txt | tee -a result.txt
+
+        git add out* result.txt
