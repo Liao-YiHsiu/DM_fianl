06811b6a6d7f33630d6977ffb4ab1c1e04a70d6b
Yi-Hsiu Liao <nee0224256667@yahoo.com.tw>
2015-01-06 20:34:14 +0800

    SVD with the same setting as in MF. F1 = 3.6%, N = 2gfjigjirgrjig

--- svd/run.m
+++ svd/run.m
@@ -1,9 +1,9 @@
 function answer = run(graph_file, train_file, test_file, out_file) 
-   N = 10;
-   cluster = parcluster('local');
-   cluster.NumWorkers=8;
-   saveProfile(cluster);
-   matlabpool open 8;
+   N = 1;
+%   cluster = parcluster('local');
+%   cluster.NumWorkers=8;
+%   saveProfile(cluster);
+%   matlabpool open 8;
 
 
    fprintf(2, 'reading Graph\n');
@@ -19,25 +19,27 @@ function answer = run(graph_file, train_file, test_file, out_file)
 
    
    % fill matrix for svd
-   matrix = -.5*ones(size(train, 2)+1, size(graph, 2));
+   matrix = 0.5*ones(size(train, 2)+size(test, 2), size(graph, 2));
+
    for j = 1:size(train, 2) 
-     matrix(j, train{j}.node) = train{j}.degree-0.5; 
+     matrix(j, train{j}.node) = train{j}.degree; 
    end
 
    for j = 1:size(test, 2)
-      matrix(end, :) = -.5; 
-      matrix(end, test{j}) = 0.5;
+      matrix(size(train,2)+j, test{j}) = 0.8;
+   end
 
-      fprintf(2, 'start svd...\n');
-      [U, S, V] = svd(matrix);
-      S2 = S;
-      S2(N+1:end, N+1:end) = 0;
-      M = U*S2*V';
+   fprintf(2, 'start svd...\n');
+   [U, S, V] = svd(matrix);
+   S2 = S;
+   S2(N+1:end, N+1:end) = 0;
+   M = U*S2*V';
 
-      M(end, test{j}) = -100;
-      
+   for j = 1:size(test, 2)
+      M(size(train, 2)+j, test{j}) = -100;
+
       tmp = zeros(size(graph,2), 2);
-      tmp(:, 1) = M(end, :)';
+      tmp(:, 1) = M(size(train,2)+j, :)';
       tmp(:, 2) = 1:size(graph,2);
 
       tmp = sortrows(tmp, -1);
@@ -46,9 +48,8 @@ function answer = run(graph_file, train_file, test_file, out_file)
 
       fprintf(fid, '%d ', answer(j, :));
       fprintf(fid, '\n');
-
    end
 
-   matlabpool close
+%   matlabpool close
 
 end
